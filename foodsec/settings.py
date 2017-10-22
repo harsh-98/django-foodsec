1ea4ee9aeae3265b5478d94a8fbc073016b4dcc6
harsh_jain <harshjniitr@gmail.com>
2017-10-22 16:29:31 +0530

    removing the static files

--- foodsec/settings.py
+++ foodsec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y115u041swcc0f#7qv=)41^es+spikpv2!$%x+kf8y4+18d7xa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'food-security.herokuapp.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
